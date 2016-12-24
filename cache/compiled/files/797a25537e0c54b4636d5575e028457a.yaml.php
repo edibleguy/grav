792961419cbd669b088204c931e47cb8f988fca8
edible <mark.lee.phd@gmail.com>
2016-12-24 06:42:04 -0800

    First commit

---
+++ cache/compiled/files/797a25537e0c54b4636d5575e028457a.yaml.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/grav/system/blueprints/config/media.yaml',
+    'modified' => 1482432293,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.MEDIA',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => NULL
+        ]
+    ]
+];
